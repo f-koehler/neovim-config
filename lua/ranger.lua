20e43e643cc1f4efd25a03c93440176b734deb09
Fabian Köhler <fabian.koehler@protonmail.ch>
2021-04-08 18:33:02 +0200

    Tweak some ranger settings.

--- lua/ranger.lua
+++ lua/ranger.lua
@@ -1,8 +1,17 @@
 -- replace netrw with ranger
-vim.g.rnvimr_ex_enable = 1
+vim.g.rnvimr_enable_ex = 1
 
 -- hide ranger after picking something
-vim.g.rnvimr_pick_enable = 1
+vim.g.rnvimr_enable_picker = 1
+
+-- enable border around floating window
+vim.g.rnvimr_draw_border = 1
 
 -- hide files that are ignored by git
 vim.g.rnvimr_hide_gitignore = 1
+
+-- close vim buffers if files are delete in ranger
+vim.g.rnvimr_enable_bw = 1
+
+-- darken background when ranger is open
+vim.g.rnvimr_shadow_winblend = 20
