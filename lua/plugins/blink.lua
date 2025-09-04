0d105332877b39b31fbcd8becabf6a73b337fe27
Fabian Koehler <fabian.koehler@proton.me>
2025-09-04 12:58:28 +0800

    chore(stylua): reformat code

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,31 +1,31 @@
 return {
-  'saghen/blink.cmp',
-  build = 'nix run .#build-plugin',
-  opts = {
-    keymap = { preset = 'default' },
+    "saghen/blink.cmp",
+    build = "nix run .#build-plugin",
+    opts = {
+        keymap = { preset = "default" },
 
-    appearance = {
-      nerd_font_variant = 'mono'
-    },
-
-    completion = { 
-        documentation = {
-            auto_show = true,
-            auto_show_delay_ms = 500,
+        appearance = {
+            nerd_font_variant = "mono",
         },
-        list = {
-            selection = {
-                preselect = true,
-                auto_insert = true,
+
+        completion = {
+            documentation = {
+                auto_show = true,
+                auto_show_delay_ms = 500,
+            },
+            list = {
+                selection = {
+                    preselect = true,
+                    auto_insert = true,
+                },
             },
         },
-    },
 
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-    },
+        sources = {
+            default = { "lsp", "path", "snippets", "buffer" },
+        },
 
-    fuzzy = { implementation = "prefer_rust_with_warning" }
-  },
-  opts_extend = { "sources.default" }
+        fuzzy = { implementation = "prefer_rust_with_warning" },
+    },
+    opts_extend = { "sources.default" },
 }
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,16 +1,16 @@
 return {
     "neovim/nvim-lspconfig",
-    dependencies = {"saghen/blink.cmp"},
+    dependencies = { "saghen/blink.cmp" },
     opts = {
         servers = {
-            lua_ls = {}
-        }
+            lua_ls = {},
+        },
     },
     config = function(_, opts)
-        local lspconfig = require('lspconfig')
+        local lspconfig = require("lspconfig")
         for server, config in pairs(opts.servers) do
-            config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+            config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
             lspconfig[server].setup(config)
         end
-    end
+    end,
 }
