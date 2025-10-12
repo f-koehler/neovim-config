2720b8294eb1a426ad03275901e9b5429cf924ce
Fabian Koehler <fabian.koehler@proton.me>
2025-10-12 15:30:36 +0800

    refactor(lsp): migrate to new way of using lsp-config

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,19 +1,10 @@
 return {
     "neovim/nvim-lspconfig",
     dependencies = { "saghen/blink.cmp" },
-    opts = {
-        servers = {
-            ansiblels = {},
-            lua_ls = {},
-            clangd = {},
-            qmlls = {},
-        },
-    },
     config = function(_, opts)
-        local lspconfig = require("lspconfig")
-        for server, config in pairs(opts.servers) do
-            config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
-            lspconfig[server].setup(config)
-        end
+        vim.lsp.config("ansiblels", {})
+        vim.lsp.config("lua_ls", {})
+        vim.lsp.config("clangd", {})
+        vim.lsp.config("qmlls", {})
     end,
 }
