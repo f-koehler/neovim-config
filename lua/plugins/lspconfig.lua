5cdd824c12235f9e00b69b4828fbe49f8f640def
Fabian Koehler <fabian@fkoehler.me>
2026-08-14 18:49:18 +0800

    fix(lsp): automatically enable LSP servers

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,11 +1,19 @@
+local servers = {
+    ansiblels = {},
+    lua_ls = {},
+    clangd = {},
+    qmlls = {},
+    ruff = {},
+    rust_analyzer = {},
+}
+
 return {
     "neovim/nvim-lspconfig",
     dependencies = { "saghen/blink.cmp" },
-    config = function(_, opts)
-        vim.lsp.config("ansiblels", {})
-        vim.lsp.config("lua_ls", {})
-        vim.lsp.config("clangd", {})
-        vim.lsp.config("qmlls", {})
-        vim.lsp.config("ruff", {})
+    config = function()
+        for server, config in pairs(servers) do
+            vim.lsp.config(server, config)
+        end
+        vim.lsp.enable(vim.tbl_keys(servers))
     end,
 }
