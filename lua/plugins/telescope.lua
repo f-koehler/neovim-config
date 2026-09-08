408525082f820b26c30f8e78b63d09659990daf9
Fabian Koehler <fabian@fkoehler.me>
2026-09-09 01:17:36 +0800

    feat: add Ctrl-p binding for telescope file picker

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,5 +10,9 @@ return {
         vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
         vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
         vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+        vim.keymap.set({ "n", "i" }, "<C-p>", function()
+            vim.cmd.stopinsert()
+            builtin.find_files()
+        end, { desc = "Telescope find files" })
     end,
 }
