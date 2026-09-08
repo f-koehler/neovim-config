7edfa8068166f2ff991a951d6b034405ede928ff
Fabian Koehler <fabian@fkoehler.me>
2026-09-09 02:01:50 +0800

    feat(conform): add some missing formatters

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,27 +1,44 @@
 local formatters_by_ft = {
+    c = { "clang-format" },
+    cmake = { "cmake_format" },
     cpp = { "clang-format" },
     css = { "prettier" },
     html = { "prettier" },
     javascript = { "prettier" },
     json = { "prettier" },
     lua = { "stylua" },
+    nix = { "nixfmt" },
     python = { "ruff" },
+    qml = { "qmlformat" },
+    rust = { "rustfmt" },
     scss = { "prettier" },
+    sh = { "shfmt" },
+    terraform = { "terraform_fmt" },
     tex = { "tex-fmt" },
+    tfvars = { "terraform_fmt" },
     toml = { "taplo" },
     typescript = { "prettier" },
     yaml = { "prettier" },
 }
 
 local mason_exclude = {
-    -- a, b
+    -- not in the mason registry
+    rustfmt = true,
+    -- not in the mason registry
+    qmlformat = true,
+}
+
+-- conform's formatter id doesn't always match the mason package name
+local mason_package_by_formatter = {
+    cmake_format = "cmakelang",
+    terraform_fmt = "terraform",
 }
 
 local tools = {}
 for _, formatters in pairs(formatters_by_ft) do
     for _, formatter in ipairs(formatters) do
         if not mason_exclude[formatter] then
-            tools[formatter] = true
+            tools[mason_package_by_formatter[formatter] or formatter] = true
         end
     end
 end
