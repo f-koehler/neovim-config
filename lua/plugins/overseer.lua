4a789d7b4f881d33ee8597e19f14fa353aa1ea54
Fabian Koehler <fabian@fkoehler.me>
2026-09-09 14:36:12 +0800

    feat(overseer): add shortcuts to run overseer task and toggle its output

--- lua/plugins/overseer.lua
+++ lua/plugins/overseer.lua
@@ -1,4 +1,9 @@
 return {
     "stevearc/overseer.nvim",
     opts = {},
+    config = function(_, opts)
+        require("overseer").setup(opts)
+        vim.keymap.set("n", "<leader>tt", "<cmd>OverseerRun<cr>", { desc = "Overseer run task" })
+        vim.keymap.set("n", "<leader>to", "<cmd>OverseerToggle<cr>", { desc = "Overseer toggle task list" })
+    end,
 }
